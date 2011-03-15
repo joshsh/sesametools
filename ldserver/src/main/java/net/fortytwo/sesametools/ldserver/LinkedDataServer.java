f8c0dcb5a75eb3c30ddaca39d6fbc71f5e85c553
Joshua Shinavier <josh@fortytwo.net>
2011-03-15 19:19:55 +0800

    LinkedDataServer tweaks.

--- ldserver/src/main/java/net/fortytwo/sesametools/ldserver/LinkedDataServer.java
+++ ldserver/src/main/java/net/fortytwo/sesametools/ldserver/LinkedDataServer.java
@@ -61,12 +61,17 @@ public class LinkedDataServer {
         if (!internalBaseURI.equals(externalBaseURI)) {
             RewriteRule outboundRewriter = new RewriteRule() {
                 public URI rewrite(final URI original) {
+                    System.out.println("outbound: " + original);
+
                     if (null == original) {
                         return null;
                     } else {
                         String s = original.stringValue();
-                        return s.startsWith(externalBaseURI)
-                                ? vf.createURI(s.replace(externalBaseURI, internalBaseURI))
+                        System.out.println("\t--> " + (s.startsWith(internalBaseURI)
+                                ? vf.createURI(s.replace(internalBaseURI, externalBaseURI))
+                                : original));
+                        return s.startsWith(internalBaseURI)
+                                ? vf.createURI(s.replace(internalBaseURI, externalBaseURI))
                                 : original;
                     }
                 }
@@ -74,12 +79,16 @@ public class LinkedDataServer {
 
             RewriteRule inboundRewriter = new RewriteRule() {
                 public URI rewrite(final URI original) {
+                    System.out.println("inbound: " + original);
                     if (null == original) {
                         return null;
                     } else {
                         String s = original.stringValue();
-                        return s.startsWith(internalBaseURI)
-                                ? vf.createURI(s.replace(internalBaseURI, externalBaseURI))
+                        System.out.println("\t--> " + (s.startsWith(externalBaseURI)
+                                ? vf.createURI(s.replace(externalBaseURI, internalBaseURI))
+                                : original));
+                        return s.startsWith(externalBaseURI)
+                                ? vf.createURI(s.replace(externalBaseURI, internalBaseURI))
                                 : original;
                     }
                 }
@@ -90,10 +99,14 @@ public class LinkedDataServer {
             schema.setRewriter(MappingSchema.Direction.OUTBOUND, outboundRewriter);
             this.sail = new MappingSail(baseSail, schema);
 
-            datasetURI = outboundRewriter.rewrite(vf.createURI(dataset));
+            datasetURI = null == dataset
+                    ? null
+                    : outboundRewriter.rewrite(vf.createURI(dataset));
         } else {
             this.sail = baseSail;
-            datasetURI = vf.createURI(dataset);
+            datasetURI = null == dataset
+                    ? null
+                    : vf.createURI(dataset);
         }
 
         // Create a new Restlet component and add a HTTP server connector to it
