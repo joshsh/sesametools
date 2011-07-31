1b9c5657092588ef7361e155121be2d48637a6bc
Peter Ansell <p_ansell@yahoo.com>
2011-07-31 12:17:49 +1000

    add test for performance of JSON parser and writer. initial test results indicate that JSON could be up to 100 times slower than Turtle parser provided by Sesame
    raw times for 32527 statements in test were 29.004 seconds and 29.886
    seconds for the two JSON related tests and 0.277 seconds for the Turtle
    parse and write test

--- rdfjson/src/test/java/net/fortytwo/sesametools/rdfjson/RDFJSONWriterTest.java
+++ rdfjson/src/test/java/net/fortytwo/sesametools/rdfjson/RDFJSONWriterTest.java
@@ -4,10 +4,14 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.openrdf.model.vocabulary.OWL;
 import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.rio.RDFParser;
 import org.openrdf.rio.RDFWriter;
+import org.openrdf.rio.turtle.TurtleParser;
+import org.openrdf.rio.turtle.TurtleWriter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.Date;
 
 /**
  * Tests the RDF/JSON writer by way of the RDF/JSON parser.
@@ -44,7 +48,41 @@ public class RDFJSONWriterTest extends RDFJSONTestBase {
 
         //j = parseAndWrite("example0.json");
     }
+    
+    public void testPerformance() throws Exception
+    {
+        Date queryStartTime = new Date();
 
+        parseAndWrite("bio2rdf-configuration.json");
+        
+        Date queryEndTime = new Date();
+        
+        long nextTotalTime = queryEndTime.getTime()-queryStartTime.getTime();
+        
+        System.out.println("testPerformance: parse and write JSON nextTotalTime="+nextTotalTime);
+
+        queryStartTime = new Date();
+
+        parseTurtleAndWriteJson("bio2rdf-configuration.ttl");
+        
+        queryEndTime = new Date();
+        
+        nextTotalTime = queryEndTime.getTime()-queryStartTime.getTime();
+        
+        System.out.println("testPerformance: parse Turtle and write JSON nextTotalTime="+nextTotalTime);
+    
+        queryStartTime = new Date();
+
+        parseTurtleAndWriteTurtle("bio2rdf-configuration.ttl");
+        
+        queryEndTime = new Date();
+        
+        nextTotalTime = queryEndTime.getTime()-queryStartTime.getTime();
+        
+        System.out.println("testPerformance: parse Turtle and write Turtle nextTotalTime="+nextTotalTime);
+    
+    }
+    
     private JSONObject parseAndWrite(final String fileName) throws Exception {
         RDFJSONParser p = new RDFJSONParser();
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -55,6 +93,42 @@ public class RDFJSONWriterTest extends RDFJSONTestBase {
             try {
                 p.parse(in, BASE_URI);
                 return JSONObject.fromObject(new String(bos.toByteArray()));
+            } finally {
+                in.close();
+            }
+        } finally {
+            bos.close();
+        }
+    }
+
+    private JSONObject parseTurtleAndWriteJson(final String fileName) throws Exception {
+        RDFParser p = new TurtleParser();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            RDFWriter w = new RDFJSONWriter(bos);
+            p.setRDFHandler(w);
+            InputStream in = RDFJSONTestBase.class.getResourceAsStream(fileName);
+            try {
+                p.parse(in, BASE_URI);
+                return JSONObject.fromObject(new String(bos.toByteArray()));
+            } finally {
+                in.close();
+            }
+        } finally {
+            bos.close();
+        }
+    }
+
+    private String parseTurtleAndWriteTurtle(final String fileName) throws Exception {
+        RDFParser p = new TurtleParser();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            RDFWriter w = new TurtleWriter(bos);
+            p.setRDFHandler(w);
+            InputStream in = RDFJSONTestBase.class.getResourceAsStream(fileName);
+            try {
+                p.parse(in, BASE_URI);
+                return new String(bos.toByteArray());
             } finally {
                 in.close();
             }
