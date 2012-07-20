070fe66320f2294efe6cf35b6033e6f827f2969d
Peter Ansell <p_ansell@yahoo.com>
2012-07-20 14:38:28 +1000

    rebase ModifiedNTriplesWriter on Rio NTriplesWriter and modify NQuadsWriter to use NTriplesUtil.append for output

--- nquads/src/main/java/net/fortytwo/sesametools/nquads/ModifiedNTriplesWriter.java
+++ nquads/src/main/java/net/fortytwo/sesametools/nquads/ModifiedNTriplesWriter.java
@@ -1,15 +1,9 @@
+/*
+ * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2011.
+ *
+ * Licensed under the Aduna BSD-style license.
+ */
 package net.fortytwo.sesametools.nquads;
-
-import org.openrdf.model.BNode;
-import org.openrdf.model.Literal;
-import org.openrdf.model.Resource;
-import org.openrdf.model.Statement;
-import org.openrdf.model.URI;
-import org.openrdf.model.Value;
-import org.openrdf.rio.RDFFormat;
-import org.openrdf.rio.RDFHandlerException;
-import org.openrdf.rio.RDFWriter;
-import org.openrdf.rio.ntriples.NTriplesUtil;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -17,158 +11,123 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.Charset;
 
+import org.openrdf.model.Statement;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.RDFWriter;
+import org.openrdf.rio.ntriples.NTriplesUtil;
+
 /**
  * An implementation of the RDFWriter interface that writes RDF documents in
  * N-Triples format. The N-Triples format is defined in <a
  * href="http://www.w3.org/TR/rdf-testcases/#ntriples">this section</a> of the
  * RDF Test Cases document.
- * <p/>
- * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2007.
- * This code has been slightly modified (by Joshua Shinavier) so as to be used with NQuadsWriter.
  */
 public class ModifiedNTriplesWriter implements RDFWriter {
 
-    /*-----------*
-      * Variables *
-      *-----------*/
+	/*-----------*
+	 * Variables *
+	 *-----------*/
 
-    protected Writer writer;
+	protected final Writer writer;
 
-    protected boolean writingStarted;
+	protected boolean writingStarted;
 
-    /*--------------*
-      * Constructors *
-      *--------------*/
+	/*--------------*
+	 * Constructors *
+	 *--------------*/
 
-    /**
-     * Creates a new NTriplesWriter that will write to the supplied OutputStream.
-     *
-     * @param out The OutputStream to write the N-Triples document to.
-     */
-    public ModifiedNTriplesWriter(OutputStream out) {
-        this(new OutputStreamWriter(out, Charset.forName("US-ASCII")));
-    }
+	/**
+	 * Creates a new NTriplesWriter that will write to the supplied OutputStream.
+	 * 
+	 * @param out
+	 *        The OutputStream to write the N-Triples document to.
+	 */
+	public ModifiedNTriplesWriter(OutputStream out) {
+		this(new OutputStreamWriter(out, Charset.forName("US-ASCII")));
+	}
 
-    /**
-     * Creates a new NTriplesWriter that will write to the supplied Writer.
-     *
-     * @param writer The Writer to write the N-Triples document to.
-     */
-    public ModifiedNTriplesWriter(Writer writer) {
-        this.writer = writer;
-        writingStarted = false;
-    }
+	/**
+	 * Creates a new NTriplesWriter that will write to the supplied Writer.
+	 * 
+	 * @param writer
+	 *        The Writer to write the N-Triples document to.
+	 */
+	public ModifiedNTriplesWriter(Writer writer) {
+		this.writer = writer;
+		writingStarted = false;
+	}
 
-    /*---------*
-      * Methods *
-      *---------*/
+	/*---------*
+	 * Methods *
+	 *---------*/
 
-    public RDFFormat getRDFFormat() {
-        return RDFFormat.NTRIPLES;
-    }
+	public RDFFormat getRDFFormat() {
+		return RDFFormat.NTRIPLES;
+	}
 
-    public void setBaseURI(String baseURI) {
-        // ignore, N-Triples doesn't support this
-    }
+	public void startRDF()
+		throws RDFHandlerException
+	{
+		if (writingStarted) {
+			throw new RuntimeException("Document writing has already started");
+		}
 
-    public void startRDF()
-            throws RDFHandlerException {
-        if (writingStarted) {
-            throw new RuntimeException("Document writing has already started");
-        }
+		writingStarted = true;
+	}
 
-        writingStarted = true;
-    }
+	public void endRDF()
+		throws RDFHandlerException
+	{
+		if (!writingStarted) {
+			throw new RuntimeException("Document writing has not yet started");
+		}
 
-    public void endRDF()
-            throws RDFHandlerException {
-        if (!writingStarted) {
-            throw new RuntimeException("Document writing has not yet started");
-        }
+		try {
+			writer.flush();
+		}
+		catch (IOException e) {
+			throw new RDFHandlerException(e);
+		}
+		finally {
+			writingStarted = false;
+		}
+	}
 
-        try {
-            writer.flush();
-        } catch (IOException e) {
-            throw new RDFHandlerException(e);
-        } finally {
-            writingStarted = false;
-        }
-    }
+	public void handleNamespace(String prefix, String name) {
+		// N-Triples does not support namespace prefixes.
+	}
 
-    public void handleNamespace(String prefix, String name) {
-        // N-Triples does not support namespace prefixes.
-    }
+	public void handleStatement(Statement st)
+		throws RDFHandlerException
+	{
+		if (!writingStarted) {
+			throw new RuntimeException("Document writing has not yet been started");
+		}
 
-    public void handleStatement(Statement st)
-            throws RDFHandlerException {
-        if (!writingStarted) {
-            throw new RuntimeException("Document writing has not yet been started");
-        }
+		try {
+			NTriplesUtil.append(st.getSubject(), writer);
+			writer.write(" ");
+			NTriplesUtil.append(st.getPredicate(), writer);
+			writer.write(" ");
+			NTriplesUtil.append(st.getObject(), writer);
+			writer.write(" .\n");
+		}
+		catch (IOException e) {
+			throw new RDFHandlerException(e);
+		}
+	}
 
-        Resource subj = st.getSubject();
-        URI pred = st.getPredicate();
-        Value obj = st.getObject();
-
-        try {
-            // SUBJECT
-            writeResource(subj);
-            writer.write(" ");
-
-            // PREDICATE
-            writeURI(pred);
-            writer.write(" ");
-
-            // OBJECT
-            if (obj instanceof Resource) {
-                writeResource((Resource) obj);
-            } else if (obj instanceof Literal) {
-                writeLiteral((Literal) obj);
-            }
-
-            writer.write(" .");
-            writeNewLine();
-        } catch (IOException e) {
-            throw new RDFHandlerException(e);
-        }
-    }
-
-    public void handleComment(String comment)
-            throws RDFHandlerException {
-        try {
-            writer.write("# ");
-            writer.write(comment);
-            writeNewLine();
-        } catch (IOException e) {
-            throw new RDFHandlerException(e);
-        }
-    }
-
-    protected void writeResource(Resource res)
-            throws IOException {
-        if (res instanceof BNode) {
-            writeBNode((BNode) res);
-        } else {
-            writeURI((URI) res);
-        }
-    }
-
-    protected void writeURI(URI uri)
-            throws IOException {
-        writer.write(NTriplesUtil.toNTriplesString(uri));
-    }
-
-    private void writeBNode(BNode bNode)
-            throws IOException {
-        writer.write(NTriplesUtil.toNTriplesString(bNode));
-    }
-
-    protected void writeLiteral(Literal lit)
-            throws IOException {
-        writer.write(NTriplesUtil.toNTriplesString(lit));
-    }
-
-    protected void writeNewLine()
-            throws IOException {
-        writer.write("\n");
-    }
+	public void handleComment(String comment)
+		throws RDFHandlerException
+	{
+		try {
+			writer.write("# ");
+			writer.write(comment);
+			writer.write("\n");
+		}
+		catch (IOException e) {
+			throw new RDFHandlerException(e);
+		}
+	}
 }
--- nquads/src/main/java/net/fortytwo/sesametools/nquads/NQuadsWriter.java
+++ nquads/src/main/java/net/fortytwo/sesametools/nquads/NQuadsWriter.java
@@ -1,11 +1,8 @@
 package net.fortytwo.sesametools.nquads;
 
 import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.ntriples.NTriplesUtil;
 import org.openrdf.model.Statement;
-import org.openrdf.model.Resource;
-import org.openrdf.model.URI;
-import org.openrdf.model.Value;
-import org.openrdf.model.Literal;
 
 import java.io.OutputStream;
 import java.io.Writer;
@@ -32,34 +29,24 @@ public class NQuadsWriter extends ModifiedNTriplesWriter {
             throw new RuntimeException("Document writing has not yet been started");
         }
 
-        Resource subj = st.getSubject();
-        URI pred = st.getPredicate();
-        Value obj = st.getObject();
-        Resource ctx = st.getContext();
-
         try {
             // SUBJECT
-            writeResource(subj);
+            NTriplesUtil.append(st.getSubject(), writer);
             writer.write(" ");
 
             // PREDICATE
-            writeURI(pred);
+            NTriplesUtil.append(st.getPredicate(), writer);
             writer.write(" ");
 
             // OBJECT
-            if (obj instanceof Resource) {
-                writeResource((Resource) obj);
-            } else if (obj instanceof Literal) {
-                writeLiteral((Literal) obj);
-            }
+            NTriplesUtil.append(st.getObject(), writer);
 
-            if (null != ctx) {
+            if (null != st.getContext()) {
                 writer.write(" ");
-                writeResource(ctx);
+                NTriplesUtil.append(st.getContext(), writer);
             }
 
-            writer.write(" .");
-            writeNewLine();
+            writer.write(" .\n");
         } catch (IOException e) {
             throw new RDFHandlerException(e);
         }
